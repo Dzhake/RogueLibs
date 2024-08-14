35aba5567fadaae1da55a281439fd66f79fff16b
zTBBz <warningstarini@yandex.ru>
2024-08-14 14:03:34 +0300

    🔖 RogueLibs v4.0.0-rc.2

--- RogueLibsCore/RogueLibs.cs
+++ RogueLibsCore/RogueLibs.cs
@@ -41,7 +41,7 @@ namespace RogueLibsCore
         /// <summary>
         ///   <para>Semantic version of RogueLibs that the current assembly is compiled with. Don't use it in BepInPlugin.</para>
         /// </summary>
-        public const string CompiledSemanticVersion = "4.0.0-rc.1";
+        public const string CompiledSemanticVersion = "4.0.0-rc.2";
         /// <summary>
         ///   <para>Currently installed and running version of RogueLibs.</para>
         /// </summary>
